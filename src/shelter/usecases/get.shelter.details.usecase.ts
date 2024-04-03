ed345c221a9856be2b578e146ba4d64e815d15ff
damiaojr91 <damiao.dsj@gmail.com>
2024-04-03 20:18:56 -0300

    Criação de projeto base e controllers

---
+++ src/shelter/usecases/get.shelter.details.usecase.ts
@@ -0,0 +1,16 @@
+import { IUseCase } from "src/domain/iusecase.interface";
+import GetShelterDetailsUseCaseOutput from "./dtos/get.shelter.details.usecase.output";
+
+export default class GetShelterDetailsUseCase implements IUseCase<null, GetShelterDetailsUseCaseOutput> {
+  run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
+    // throw new Error("Method not implemented.");
+    return Promise.resolve(new GetShelterDetailsUseCaseOutput({
+      shelterName: 'DataPet',
+      shelterEmail: 'datapet@datapet.com',
+      shelterPhone: '1912345678',
+      shelterWhatsApp: '19123456789',
+      createdAt: new Date(),
+      updateAt: new Date()
+    }))
+  }
+}
