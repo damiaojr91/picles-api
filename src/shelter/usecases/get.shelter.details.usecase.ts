4b018dc709ae2f8e450941450d39d1937300514b
damiaojr91 <damiao.dsj@gmail.com>
2024-04-05 22:32:10 -0300

    Atualizando método de conexão com o banco de dados

--- src/shelter/usecases/get.shelter.details.usecase.ts
+++ src/shelter/usecases/get.shelter.details.usecase.ts
@@ -1,19 +1,25 @@
 import { IUseCase } from 'src/domain/iusecase.interface';
 import GetShelterDetailsUseCaseOutput from './dtos/get.shelter.details.usecase.output';
+import { Inject } from '@nestjs/common';
+import ShelterTokens from '../shelter.tokens';
+import IShelterRepository from '../interfaces/shelter.repository.interface';
 
-export default class GetShelterDetailsUseCase implements IUseCase<null, GetShelterDetailsUseCaseOutput>
-{
-  run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
-    // throw new Error("Method not implemented.");
-    return Promise.resolve(
-      new GetShelterDetailsUseCaseOutput({
-        shelterName: 'DataPet',
-        shelterEmail: 'datapet@datapet.com',
-        shelterPhone: '1912345678',
-        shelterWhatsApp: '19123456789',
-        createdAt: new Date(),
-        updateAt: new Date(),
-      }),
-    );
+export default class GetShelterDetailsUseCase implements IUseCase<null, GetShelterDetailsUseCaseOutput> {
+  constructor(
+    @Inject(ShelterTokens.shelterRepository)
+    private readonly ShelterRepository: IShelterRepository,
+  ) {}
+
+  async run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
+    const shelter = await this.ShelterRepository.get();
+
+    return new GetShelterDetailsUseCaseOutput({
+      shelterName: shelter.name,
+      shelterEmail: shelter.email,
+      shelterPhone: shelter.phone,
+      shelterWhatsApp: shelter.whatsApp,
+      createdAt: shelter.createdAt,
+      updateAt: shelter.updatedAt,
+    });
   }
 }
