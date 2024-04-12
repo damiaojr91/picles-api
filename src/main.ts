b3c02f8b991ade69bb35875c9356df37f43f1534
damiaojr91 <damiao.dsj@gmail.com>
2024-04-12 20:55:23 -0300

    Adicionando Swagger e habilitando CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // CORS
+  app.enableCors();
+
+  // Swagger
+  // localhost:3000/api
+  const config = new DocumentBuilder()
+    .setTitle('Picles')
+    .setDescription('A shelter api')
+    .setVersion('1.0')
+    .addTag('picles')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3005);
 }
 bootstrap();
